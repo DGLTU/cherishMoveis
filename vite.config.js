1626c09665dda58261be6db1647dd6725d562d0c
DGLtu <dg56234@gmail.com>
2022-12-08 20:37:56 +0800

    modify base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/instrument/',
+  base: '/cherishMoveis/',
   plugins: [vue()],
   resolve: {
     alias: {
